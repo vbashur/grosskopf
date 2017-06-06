819f6747de34faa558fdc65bf08f69dca110a2a9
vbashur <vbashur@yandex.ru>
2017-06-06 23:25:27 +0200

    Added login page, redirect to login page for specific requests

--- authservice/src/main/java/com/vbashur/authservice/configuration/LoginConfiguration.java
+++ authservice/src/main/java/com/vbashur/authservice/configuration/LoginConfiguration.java
@@ -1,15 +1,19 @@
 package com.vbashur.authservice.configuration;
-/*
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-*/
-//@Configuration
-public class LoginConfiguration /*extends WebSecurityConfigurerAdapter*/ {
-/*
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class LoginConfiguration extends WebSecurityConfigurerAdapter {
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -19,9 +23,10 @@ public class LoginConfiguration /*extends WebSecurityConfigurerAdapter*/ {
         http
                 .formLogin().loginPage("/login").permitAll()
                 .and()
-                .requestMatchers().antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access")
+                .requestMatchers().antMatchers("/login", "/index", "/oauth/authorize", "/oauth/confirm_access")
                 .and()
-                .authorizeRequests().anyRequest().authenticated();
+                .authorizeRequests()
+                .anyRequest().authenticated();
         // @formatter:on
     }
 
@@ -31,7 +36,7 @@ public class LoginConfiguration /*extends WebSecurityConfigurerAdapter*/ {
         auth.inMemoryAuthentication()
                 .withUser("user").password("user").roles("USER")
                 .and().withUser("admin").password("admin").roles("ADMIN");
-    }*/
+    }
 }
 
 
