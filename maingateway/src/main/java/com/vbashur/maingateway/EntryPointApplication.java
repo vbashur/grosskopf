6f51ad1199043bcc27226a0c71283e0f722ac8f9
vbashur <vbashur@yandex.ru>
2017-06-12 23:34:31 +0200

    Update for main gateway to forward requests to auth servers

--- maingateway/src/main/java/com/vbashur/maingateway/EntryPointApplication.java
+++ maingateway/src/main/java/com/vbashur/maingateway/EntryPointApplication.java
@@ -3,13 +3,31 @@ package com.vbashur.maingateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 @EnableZuulProxy
-public class EntryPointApplication {
+public class EntryPointApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(EntryPointApplication.class, args);
     }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .logout().and()
+                .authorizeRequests()
+                .antMatchers("/login", "/index", "/oauth/authorize", "/oauth/confirm_access", "/test").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        // @formatter:on
+    }
+
 
 }
